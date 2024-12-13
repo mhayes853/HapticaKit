a2e5edf2e85b14cd02f0dd9b6cf5e45035932036
mhayes853 <matthewha853@gmail.com>
2024-12-13 11:50:52 -0800

    Fix

--- jest/primitives.js
+++ jest/primitives.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-const { HapticaExtensionError } = require("../src/error");
+const { HapticaExtensionError } = require("../index");
 
 const DEFAULT_PATTERN = {
   name: "",
